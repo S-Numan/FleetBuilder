c64c804971c97bdbd26f05c53344381055b0c8c1
S-Numan <TheSadNumanator@gmail.com>
2025-07-03 23:11:39 -0700

    .

--- src/fleetBuilder/util/ModifyInternalVariants.kt
+++ src/fleetBuilder/util/ModifyInternalVariants.kt
@@ -6,7 +6,7 @@ import starficz.ReflectionUtils.getFieldsMatching
 
 object ModifyInternalVariants {
 
-    const val safteyPrefix = "FBV_"//FBT = Fleet Builder Variant
+    const val safteyPrefix = "FBV_"//FBV = Fleet Builder Variant
     private fun getInternalVariantsMap(): HashMap<String, HullVariantSpec> {
         val eep = Class.forName("com.fs.starfarer.loading.new")
         val erp = eep.getFieldsMatching(fieldAccepts = Map::class.java).firstOrNull() ?: return hashMapOf()
