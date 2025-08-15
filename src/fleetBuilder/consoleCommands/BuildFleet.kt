f65410d6afdfa456471c047de6f45856e36b5fcf
S-Numan <TheSadNumanator@gmail.com>
2025-08-14 20:11:42 -0700

    corvus 0.98 special hullmod items

--- src/fleetBuilder/consoleCommands/BuildFleet.kt
+++ src/fleetBuilder/consoleCommands/BuildFleet.kt
@@ -1,12 +1,16 @@
 package fleetBuilder.consoleCommands
 
 import com.fs.starfarer.api.Global
+import com.fs.starfarer.api.campaign.CargoAPI
+import com.fs.starfarer.api.campaign.SpecialItemData
 import com.fs.starfarer.api.impl.campaign.ids.Industries
+import com.fs.starfarer.api.impl.campaign.ids.Items
 import fleetBuilder.features.CommanderShuttle.addPlayerShuttle
 import org.lazywizard.console.BaseCommand
 import org.lazywizard.console.CommonStrings
 import org.lazywizard.console.Console
 import org.lazywizard.console.commands.*
+import org.magiclib.kotlin.getStorageCargo
 
 
 class BuildFleet : BaseCommand {
@@ -15,6 +19,18 @@ class BuildFleet : BaseCommand {
             Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY)
             return BaseCommand.CommandResult.WRONG_CONTEXT
         }
+
+        val abandonedStation = Global.getSector().getStarSystem("corvus").getEntityById("corvus_abandoned_station")
+            ?: run {
+                Console.showMessage("Couldn't get the corvus abandoned station")
+                return BaseCommand.CommandResult.ERROR
+            }
+        val cargo = abandonedStation.market.getStorageCargo()
+            ?: run {
+                Console.showMessage("Couldn't get cargo")
+                return BaseCommand.CommandResult.ERROR
+            }
+
 
         val playerFleet = Global.getSector().playerFleet.fleetData
 
@@ -34,9 +50,10 @@ class BuildFleet : BaseCommand {
         GoTo().runCommand("corvus_abandoned_station", context)
 
         //Give the corvus abandoned station a spaceport
-        Global.getSector().getStarSystem("corvus").getEntityById("corvus_abandoned_station").market.addIndustry(
+        abandonedStation.market.addIndustry(
             Industries.SPACEPORT
         )
+
         AddCredits().runCommand("30000000", context)
         AddXP().runCommand("30000000", context)
         AddStoryPoints().runCommand("500", context)
@@ -44,6 +61,21 @@ class BuildFleet : BaseCommand {
         AllHullmods().runCommand("", context)
         AllWeapons().runCommand("", context)
         AllWings().runCommand("", context)
+
+
+        fun addSpecialUpToLimit(cargo: CargoAPI, itemId: String, limit: Float) {
+            val existingAmount = cargo.getQuantity(CargoAPI.CargoItemType.SPECIAL, SpecialItemData(itemId, null))
+            if (existingAmount < limit) {
+                val toAdd = limit - existingAmount
+                cargo.addSpecial(SpecialItemData(itemId, null), toAdd)
+            }
+        }
+
+        addSpecialUpToLimit(cargo, Items.FRAGMENT_FABRICATOR, 10000f)
+        addSpecialUpToLimit(cargo, Items.THREAT_PROCESSING_UNIT, 10000f)
+        addSpecialUpToLimit(cargo, Items.SHROUDED_THUNDERHEAD, 10000f)
+        addSpecialUpToLimit(cargo, Items.SHROUDED_MANTLE, 10000f)
+        addSpecialUpToLimit(cargo, Items.SHROUDED_LENS, 10000f)
 
         Console.showMessage("\nFleet builder done")
 
