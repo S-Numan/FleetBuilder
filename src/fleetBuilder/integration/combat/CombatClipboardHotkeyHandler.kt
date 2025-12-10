3bd0a66090b661f484b5965ae7d762765d8c8028
S-Numan <TheSadNumanator@gmail.com>
2025-12-10 00:22:20 -0800

    Spawn ship in battles

--- src/fleetBuilder/integration/combat/CombatClipboardHotkeyHandler.kt
+++ src/fleetBuilder/integration/combat/CombatClipboardHotkeyHandler.kt
@@ -1,32 +1,35 @@
 package fleetBuilder.integration.combat
 
+import com.fs.starfarer.api.GameState
 import com.fs.starfarer.api.Global
 import com.fs.starfarer.api.combat.CombatEngineAPI
 import com.fs.starfarer.api.combat.EveryFrameCombatPlugin
-import com.fs.starfarer.api.combat.ShipVariantAPI
+import com.fs.starfarer.api.combat.ShipHullSpecAPI
 import com.fs.starfarer.api.combat.ViewportAPI
+import com.fs.starfarer.api.fleet.FleetMemberAPI
+import com.fs.starfarer.api.fleet.FleetMemberType
+import com.fs.starfarer.api.impl.SharedUnlockData
 import com.fs.starfarer.api.input.InputEventAPI
 import com.fs.starfarer.api.input.InputEventType
-import com.fs.starfarer.loading.specs.HullVariantSpec
+import com.fs.starfarer.api.mission.FleetSide
 import fleetBuilder.config.FBTxt
 import fleetBuilder.config.ModSettings
 import fleetBuilder.config.ModSettings.fleetClipboardHotkeyHandler
-import fleetBuilder.persistence.fleet.DataFleet
-import fleetBuilder.persistence.fleet.DataFleet.createCampaignFleetFromData
-import fleetBuilder.persistence.fleet.DataFleet.getFleetDataFromFleet
 import fleetBuilder.persistence.member.DataMember
+import fleetBuilder.persistence.member.DataMember.buildMemberFull
 import fleetBuilder.persistence.variant.DataVariant
 import fleetBuilder.persistence.variant.DataVariant.buildVariantFull
 import fleetBuilder.util.*
 import fleetBuilder.util.FBMisc.handleRefitCopy
 import fleetBuilder.util.FBMisc.handleRefitPaste
 import fleetBuilder.util.ReflectionMisc.getCodexDialog
-import fleetBuilder.util.ReflectionMisc.getCoreUI
 import fleetBuilder.variants.MissingElements
 import fleetBuilder.variants.reportMissingElementsIfAny
 import org.lwjgl.input.Keyboard
-import starficz.findChildWithMethod
+import org.lwjgl.input.Mouse
+import org.lwjgl.util.vector.Vector2f
 import java.awt.Color
+import kotlin.math.atan2
 
 internal class CombatClipboardHotkeyHandler : EveryFrameCombatPlugin {
 
@@ -61,19 +64,16 @@ internal class CombatClipboardHotkeyHandler : EveryFrameCombatPlugin {
                             event.consume()
                             continue
                         }
-                        if (Global.getCombatEngine().isSimulation) {
-                            val core = getCoreUI() ?: return
-                            val simulatorUI = core.findChildWithMethod("enableAdvanced") ?: return
+                        val engine = Global.getCombatEngine() ?: return
+                        if (engine.isSimulation || (Global.getCurrentState() == GameState.COMBAT && ModSettings.cheatsEnabled())) {
+                            if (ReflectionMisc.isCodexOpen() || engine.combatUI.isShowingCommandUI)
+                                return
 
-                            val variantIdList = mutableListOf<String>()
-
-
-                            ModifyInternalVariants.clearAllModifiedVariants()
                             var element: Any? = null
 
                             if (event.eventValue == Keyboard.KEY_V) {
                                 val data = ClipboardMisc.extractDataFromClipboard() ?: return
-                                if (data is DataVariant.ParsedVariantData || data is DataMember.ParsedMemberData || data is DataFleet.ParsedFleetData) {
+                                if (data is DataVariant.ParsedVariantData || data is DataMember.ParsedMemberData) {
                                     //
                                 } else {
                                     DisplayMessage.showMessage(FBTxt.txt("no_valid_data_in_clipboard"), Color.YELLOW)
@@ -82,66 +82,108 @@ internal class CombatClipboardHotkeyHandler : EveryFrameCombatPlugin {
                                 }
 
                                 element = data
-                            } else if (event.eventValue == Keyboard.KEY_D) {
-                                val playerFleet = Global.getSector().playerFleet
-
-                                element = getFleetDataFromFleet(playerFleet)
                             }
 
-                            var missing = MissingElements()
+                            val missing = MissingElements()
+                            var member: FleetMemberAPI? = null
 
                             when (element) {
                                 is DataVariant.ParsedVariantData -> {
                                     val variant = buildVariantFull(element, missing = missing)
-
-                                    ModifyInternalVariants.setModifiedInternalVariant(variant as HullVariantSpec)
-                                    variantIdList.add("${ModifyInternalVariants.safteyPrefix}${variant.hullVariantId}")
+                                    member = Global.getFactory().createFleetMember(FleetMemberType.SHIP, variant)
+                                    member.crewComposition.addCrew(member.neededCrew)
+                                    member.repairTracker.cr = 0.7f
                                 }
 
                                 is DataMember.ParsedMemberData -> {
-                                    var variant: ShipVariantAPI? = null
                                     if (element.variantData != null)
-                                        variant = buildVariantFull(element.variantData, missing = missing)
-
-                                    ModifyInternalVariants.setModifiedInternalVariant(variant as HullVariantSpec)
-                                    variantIdList.add("${ModifyInternalVariants.safteyPrefix}${variant.hullVariantId}")
+                                        member = buildMemberFull(element, missing = missing)
+                                    member!!.crewComposition.addCrew(member.neededCrew)
                                 }
+                            }
+                            if (member == null)
+                                return
 
-                                is DataFleet.ParsedFleetData -> {
-                                    val fleet = createCampaignFleetFromData(element, false, missing = missing)
+                            if (missing.hullIds.isNotEmpty()) {
+                                DisplayMessage.showError("Could not find HullSpec with ID '${missing.hullIds.first()}'")
+                                return
+                            }
 
-                                    fleet.fleetData.membersListCopy.forEach { member ->
-                                        ModifyInternalVariants.setModifiedInternalVariant(member.variant as HullVariantSpec)
-                                        variantIdList.add("${ModifyInternalVariants.safteyPrefix}${member.variant.hullVariantId}")
-                                    }
-                                }
+                            if (!ModSettings.cheatsEnabled() && member.hullSpec.hints.contains(ShipHullSpecAPI.ShipTypeHints.HIDE_IN_CODEX)) {
+                                DisplayMessage.showMessage("Cannot spawn ship of hull '${member.hullSpec.hullName}'. It is hidden in the codex which suggests it cannot be simulated.", Color.YELLOW)
+                                return
+                            }
+                            if (!ModSettings.cheatsEnabled() && member.hullSpec.hasTag("codex_unlockable") && !SharedUnlockData.get().isPlayerAwareOfShip(member.hullId)) {
+                                DisplayMessage.showMessage("Cannot spawn ship of hull '${member.hullSpec.hullName}'. The player is not aware of this hull.", Color.YELLOW)
+                                return
                             }
 
                             reportMissingElementsIfAny(missing)
 
+                            val viewport = engine.viewport ?: return
+                            val sx = Mouse.getX().toFloat()
+                            val sy = Mouse.getY().toFloat()
+                            val worldX = viewport.convertScreenXToWorldX(sx)
+                            val worldY = viewport.convertScreenYToWorldY(sy)
+                            val loc = Vector2f(worldX, worldY)
 
-                            //simulatorUI.invoke("õ00000", true)//Remake Codex UI to show new members
+                            fun spawnAt(
+                                engine: CombatEngineAPI,
+                                member: FleetMemberAPI,
+                                side: FleetSide,
+                                loc: Vector2f
+                            ) {
+                                val fm = engine.getFleetManager(side)
 
-                            simulatorUI.safeInvoke("updateReserves", variantIdList, null)
+                                var facing: Float
 
-                            /*
-                            val uiFields = simulatorUI.getFieldsMatching(fieldAssignableTo = UIPanelAPI::class.java)
-                            val tempArray = uiFields.map { field ->
-                                simulatorUI.get(field.name) as? UIPanelAPI
+                                // Player ship location
+                                //val player = engine.playerShip
+                                //if (player == null) {
+                                facing = -90f
+                                /*} else {
+                                    val px = player.location.x
+                                    val py = player.location.y
+
+                                    // Angle from spawn location → player ship
+                                    facing = Vector2f.sub(
+                                        Vector2f(px, py),
+                                        loc,
+                                        null
+                                    ).let { vec ->
+                                        Math.toDegrees(atan2(vec.y.toDouble(), vec.x.toDouble())).toFloat()
+                                    }
+                                }*/
+
+                                // Spawn with that facing direction
+                                member.owner = side.ordinal
+                                val cr = member.repairTracker.cr
+
+                                val suppress: Boolean = engine.getFleetManager(side).isSuppressDeploymentMessages
+                                engine.getFleetManager(side).isSuppressDeploymentMessages = true
+
+                                val ship = fm.spawnFleetMember(member, loc, facing, 0f)
+
+                                engine.getFleetManager(side).isSuppressDeploymentMessages = suppress
+
+                                ship.crAtDeployment = cr
+                                ship.currentCR = cr
+                                ship.owner = member.owner
+                                ship.shipAI.forceCircumstanceEvaluation()
+
+                                /*engine.addFloatingText(
+                                    loc,
+                                    "Spawned: ${member.hullSpec.hullName}",
+                                    18f,
+                                    Color.CYAN,
+                                    ship,
+                                    0f,
+                                    0f
+                                )*/
                             }
-                            //val bottomShipSelector = tempArray.find { it.getMethodsMatching(name = "isShowDmods").isNotEmpty() }
 
-                            val combatFleetManagers = simulatorUI.getFieldsMatching(fieldAssignableTo = CombatFleetManager::class.java)
-                            val combatFleetManager = combatFleetManagers.getOrNull(1)?.get(simulatorUI) as? CombatFleetManager
-                            val reserves = combatFleetManager?.reserves as LinkedHashSet<FleetMember>*/
+                            spawnAt(engine, member, FleetSide.ENEMY, loc)
 
-
-                            //val plugin = Misc.getSimulatorPlugin() ?: return
-                            //if (plugin !is SimulatorPluginImpl) return
-                            //plugin.addCustomOpponents(variantIdList)
-
-
-                            DisplayMessage.showMessage("Replaced simulator reserves")
                             event.consume()
                         } else if (event.eventValue == Keyboard.KEY_V) {
                             if (!ReflectionMisc.isCodexOpen() && !DialogUtil.isPopUpUIOpen())
